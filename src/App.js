aca07dc717c30240fb3507bc0cb4f521b3af5cbe
juancereceda <juanmcereceda@gmail.com>
2021-12-23 19:36:50 -0300

    nuevo archivo para capitalizar palabras

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         After class GIT
-        </p>
-      </header>
+      <h1>Proyecto React</h1>
     </div>
   );
 }
