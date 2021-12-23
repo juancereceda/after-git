bdb37834e82d8e21e36523ada56060be315552ef
juancereceda <juanmcereceda@gmail.com>
2021-12-23 19:26:04 -0300

    probando de nuevo

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
          After class GIT
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
